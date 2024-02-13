f919e5ce04700c8b581bc0f4d8e8363d6e5a9d72
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-13 16:26:44 -0500

    updated Navbar with mobile responsive

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,23 +1,25 @@
-import Image from "next/image";
+"use client";
 import Link from "next/link";
 import SearchInput from "./SearchInput";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [dropDownMenu, setDropDownMenu] = useState<boolean>(false);
   return (
-    <div className="sticky top-0 flex w-full items-center justify-between">
+    <div className="sticky top-0 flex w-full items-center justify-between px-5 lg:px-0">
       {/* LOGO */}
       <div className="group flex items-center">
         <Link href="/">
-          <span className="text-3xl font-bold text-red-500 duration-1000 group-hover:text-white">
+          <span className="text-3xl font-bold text-red-500 duration-1000 group-hover:text-white md:text-5xl">
             Movie
           </span>
-          <span className="ml-1 text-xl font-semibold duration-1000 group-hover:text-red-500">
+          <span className="ml-1 text-xl font-semibold duration-1000 group-hover:text-red-500 md:text-3xl">
             Hub
           </span>
         </Link>
       </div>
       {/* NAV LINK */}
-      <div className="flex items-center gap-10">
+      <div className="hidden items-center gap-10 lg:flex">
         <Link
           href="/"
           className="text-xl font-semibold duration-700 hover:text-red-500"
@@ -32,14 +34,39 @@ const Navbar = () => {
         </Link>
       </div>
       {/* SEARCH-BAR AND AVATAR */}
-      <div className="flex items-center justify-end gap-10">
+      <div className="flex items-center justify-end gap-16 lg:gap-5">
         <SearchInput />
         <img
           src="https://avatar.iran.liara.run/public/boy?username=loginuser"
           alt="avatar"
-          className="h-10 w-10 rounded-full hover:scale-150 duration-500"
+          className="h-14 w-14 rounded-full duration-500 hover:scale-150"
+          onClick={() => setDropDownMenu(!dropDownMenu)}
         />
       </div>
+      {dropDownMenu && (
+        <div className="absolute right-10 top-20 z-20 flex min-w-40 flex-col gap-4 rounded-xl bg-white/5 p-4 backdrop-blur-lg">
+          <Link
+            onClick={() => setDropDownMenu(false)}
+            href="/"
+            className="text-end text-lg font-semibold text-white duration-700 hover:text-red-500"
+          >
+            Home
+          </Link>
+          <Link
+            onClick={() => setDropDownMenu(false)}
+            href="/my-list"
+            className="text-end text-lg font-semibold text-white duration-700 hover:text-red-500"
+          >
+            My List
+          </Link>
+          <p
+            onClick={() => setDropDownMenu(false)}
+            className="text-end text-lg font-semibold text-white duration-700 hover:text-red-500"
+          >
+            Log Out
+          </p>
+        </div>
+      )}
     </div>
   );
 };
--- components/Navbar/SearchInput.tsx
+++ components/Navbar/SearchInput.tsx
@@ -1,14 +1,32 @@
+"use client";
+import { useState } from "react";
 import { IoSearch } from "react-icons/io5";
 
 const SearchInput = () => {
+  const [dropSearch, setDropSearch] = useState<boolean>(false);
+  const [searchTerm, setSearchTerm] = useState<string>("");
   return (
-    <div className="group flex items-center justify-center gap-4">
-      <IoSearch className="scale-[2] opacity-80 duration-500 hover:opacity-100" />
+    <div className="group relative flex items-center justify-center gap-4">
+      <IoSearch
+        className="scale-[2] opacity-50 duration-500 group-hover:opacity-100"
+        onClick={() => setDropSearch(!dropSearch)}
+      />
       <input
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         type="text"
         placeholder="Search Movie ..."
-        className="bg-transparent opacity-80 outline-none duration-500 hover:opacity-100"
+        className="hidden bg-transparent opacity-50 outline-none duration-500 group-hover:opacity-100 lg:inline"
       />
+      {dropSearch && (
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          type="text"
+          placeholder="Search Movie ..."
+          className="absolute top-16 z-10 min-w-full rounded-lg bg-white/5 p-4 opacity-50 outline-none backdrop-blur-lg duration-500 group-hover:opacity-100"
+        />
+      )}
     </div>
   );
 };
