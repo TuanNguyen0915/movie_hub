bbb7489c2867761476351e297adae8852d498c05
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-13 21:16:11 -0500

    create function to get movies by genres

---
+++ actions/getMoviesData.ts
@@ -0,0 +1,17 @@
+import { getMoviesApi } from "@/services/getMoviesApi";
+
+export const fetchTrendingMovies = async () => {
+  const data = await getMoviesApi("trending/all/week?language=en-US");
+  return data.results;
+};
+
+export const fetchGenresMovies = async () => {
+  const data = await getMoviesApi("/genre/movie/list?language=en");
+  const genres = data.genres;
+
+  for (const genre of genres) {
+    const data = await getMoviesApi(`/discover/movie?with_genres=${genre.id}`);
+    genre.movies = data.results;
+  }
+  return genres;
+};
--- actions/trendingMovie.ts
+++
@@ -1,9 +0,0 @@
-import { getMoviesApi } from "@/services/getMoviesApi";
-
-export const fetchTrendingMovies = async () => {
-  const data = await getMoviesApi("trending/all/week?language=en-US");
-  return data.results;
-};
-
-
-fetchTrendingMovies()
