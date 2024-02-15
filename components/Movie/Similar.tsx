0ff903b19216f35ab1a57daeeaf8b2b0eef7912a
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-15 16:35:11 -0500

    created Similar movie component

---
+++ components/Movie/Similar.tsx
@@ -0,0 +1,25 @@
+"use client";
+import {getSimilar } from "@/actions/getMoviesData";
+import { IMovie } from "@/types";
+import React, { useEffect, useState } from "react";
+import HeroCard from "../Hero/HeroCard";
+
+const Similar = ({movieId }: { movieId: number | any }) => {
+  const [movies, setMovies] = useState<IMovie[]>();
+  useEffect(() => {
+    const fetchData = async () => {
+      const resData = await getSimilar(movieId);
+      setMovies(resData);
+    };
+    fetchData();
+  }, [movieId]);
+
+  console.log(movies)
+  return (
+    <>
+    {movies && <HeroCard trending={movies}/>}
+    </>
+  );
+};
+
+export default Similar;
