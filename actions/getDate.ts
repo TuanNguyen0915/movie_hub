4f9d36609d9844ba9727f86cfd10b5d47bfa8c3a
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-16 15:10:58 -0500

    fixed deployment

--- actions/getDate.ts
+++ actions/getDate.ts
@@ -1,11 +1,5 @@
-export const getReleaseDate = (date) => {
-  let filterDate = date.results?.find((result) => result.iso_3166_1 === "US");
-  filterDate = filterDate.release_dates[0].release_date;
-  let getDate = new Date(filterDate);
-  return getDate.getFullYear();
-};
 
-export const getCreateDate = (date) => {
+export const getCreateDate = (date:any) => {
   let getDate = new Date(date);
   let formatDate = `${getDate.getMonth()} - ${getDate.getDate()} - ${getDate.getFullYear()}`
   return formatDate
